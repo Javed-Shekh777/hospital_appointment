9ca438d0a01a65788aaafaddca692b0ebbc944e5
javed-shekh777 <mdjavedshekh777>
2025-03-21 12:50:35 +0530

    Fsecure_asset

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -19,12 +19,12 @@
         rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/vendor/aos/aos.css" rel="stylesheet') }}">
-    <link href="{{ asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/vendor/aos/aos.css" rel="stylesheet') }}">
+    <link href="{{ secure_asset('assets/vendor/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 
     <!-- Main CSS File -->
-    <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/responsive.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/css/main.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/css/responsive.css') }}" rel="stylesheet">
 </head>
